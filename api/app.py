1c36dfd8a56e517e32da8dfb3da38421fd139da0
hepona <hepona10@gmail.com>
2024-12-05 00:17:59 +0100

    wip6: optimise model

---
+++ api/app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello, world!"
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
