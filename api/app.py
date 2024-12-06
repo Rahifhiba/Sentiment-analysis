cee715d9afb7dc20251bd28f8a32bfbe900f03aa
hepona <hepona10@gmail.com>
2024-12-06 12:12:59 +0100

    wip8: flask webapp implementation - show fetched tweets

--- api/app.py
+++ api/app.py
@@ -1,57 +1,43 @@
 #!/usr/bin/env python3
 from flask import Flask, render_template, request
 import joblib
-from scrapping import *
-from twikit import Client
-from dotenv import load_dotenv
-import os
+from scrapping import get_tweets, initialize_client
+from flask.cli import AppGroup
+import pandas as pd
 import asyncio
-from client_config import client
-
 
 app = Flask(__name__)
+
 tfidf_vectorizer = joblib.load("model/TF-IDF Vector.pkl")
 sentiment_model = joblib.load("model/LR sentiment analysis.joblib")
-pre = TextPreprocessor()
+
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
+client = loop.run_until_complete(initialize_client())
 
 
-# Load environment variables
-def config():
-    load_dotenv()
 
-
-config()
-
-# USERNAME = os.getenv("USERNAME")
-# EMAIL = os.getenv("EMAIL")
-# PASSWORD = os.getenv("PASSWORD")
-
-
-# client = Client("en-US")
-# client.login(auth_info_1=USERNAME, auth_info_2=EMAIL, password=PASSWORD)
-# print("login succeeded")
-
-
-@app.route("/")
+@app.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
 
 
 @app.route("/analyze", methods=["POST"])
-async def analyze_sentiment():
+def analyze_data():
     topic = request.form.get("topic")
     if not topic:
-        return render_template("index.html", error="No topic provided")
-    # try:
-    df = await get_tweets_async(topic, client)  # Fetch tweets asynchronously
-    tweets_data = df.to_dict(
-        orient="records"
-    )  # Convert DataFrame to a list of dictionaries
-    return render_template("index.html", tweets=tweets_data, topic=topic)
-    # except Exception as e:
-    #     return render_template("index.html", error=f"Error fetch tweets: {e}")
+        return render_template("index.html", error="Please enter a topic.")
+
+    try:
+        tweets_data = loop.run_until_complete(get_tweets(topic, client))
+        if tweets_data.empty:
+            return render_template("index.html", error="No tweets found for the topic.")
+    except Exception as e:
+        return render_template("index.html", error=f"Error fetching tweets: {e}")
+    
+    tweets_html = tweets_data.to_html(index=False, classes="tweets-table")
+    return render_template("index.html", data=tweets_html, topic=topic)
 
 
 if __name__ == "__main__":
-    # asyncio.run(login_agent())  # Ensure login before starting the server
-    app.run(debug=True)
+    app.run()
